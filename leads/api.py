668581f365b48485a6139b7a0098fa5e5303d099
Flutter Zone <letsflatter@gmail.com>
2021-01-03 18:07:38 +0000

    Added the LeadSerializer and the LeadViewSet

---
+++ leads/api.py
@@ -0,0 +1,9 @@
+from leads.models import Lead
+from rest_framework import viewsets, permissions
+from .serializers import LeadSerializer
+
+# Lead viewset
+class LeadViewSet(viewset.ModelViewSet):
+    queryset = Lead.objects.all()
+    permission_classes = [permissions.AllowAny]
+    serializer_class = LeadSerializer
