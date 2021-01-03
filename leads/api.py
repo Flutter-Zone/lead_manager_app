1c01f251fa5a717339e62caf4593da44138c7da0
Flutter Zone <letsflatter@gmail.com>
2021-01-03 19:10:29 +0000

    Added the frontend app and urls.py for the leads app

--- leads/api.py
+++ leads/api.py
@@ -3,7 +3,7 @@ from rest_framework import viewsets, permissions
 from .serializers import LeadSerializer
 
 # Lead viewset
-class LeadViewSet(viewset.ModelViewSet):
+class LeadViewSet(viewsets.ModelViewSet):
     queryset = Lead.objects.all()
     permission_classes = [permissions.AllowAny]
     serializer_class = LeadSerializer
